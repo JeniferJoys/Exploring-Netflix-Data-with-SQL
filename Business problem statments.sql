ecb30431cd925d0ee3b15e050d189eda291a6b5c
JeniferJoys <jenifercruz399@gmail.com>
2024-09-28 01:13:09 +0530

    Add files via upload

---
+++ Business problem statments.sql
@@ -0,0 +1,17 @@
+-- 15 Business Problems & Solutions
+
+1. Compare the total number of Movies and TV Shows.
+2. Determine the most frequent rating for Movies and TV Shows.
+3. List all Movies released in a specified year (e.g., 2020).
+4. Identify the top 5 countries with the most content available on Netflix.
+5. Find the longest Movie in the dataset.
+6. Retrieve content added in the past 5 years.
+7. List all Movies and TV Shows directed by Rajiv Chilaka.
+8. Identify all TV Shows that have more than 5 seasons.
+9. Count the number of content items in each genre.
+10. Calculate the average number of content releases in India on Netflix by year, and return the top 5 years with the highest averages.
+11. List all Movies classified as documentaries.
+12. Find all content that lacks a director.
+13. Determine how many Movies Salman Khan has appeared in over the last 10 years.
+14. Identify the top 10 actors who have starred in the most Movies produced in India.
+15. Categorize content based on the presence of keywords 'kill' and 'violence' in the description.
